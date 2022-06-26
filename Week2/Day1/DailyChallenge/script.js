d229bb7853a3dd37e600a14909fee3e7b071bdad
ElisavetaAlekseeva <ealekseeva515@gmail.com>
2022-06-26 21:18:47 +0300

    added conditions

--- Week2/Day1/DailyChallenge/script.js
+++ Week2/Day1/DailyChallenge/script.js
@@ -1,31 +1,38 @@
 // Exercise 1
 
-// let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
 
-// // 1
-// let a = fruits.shift();
-// console.log(a);
-// console.log(fruits);
 
-// // 2
-// console.log(fruits.sort());
+// Remove Banana from the array.
+// Sort the array in alphabetical order.
+// Add “Kiwi” to the end of the array.
+// Remove “Apples” from the array. Don’t use the same method as in part 1.
+// Sort the array in reverse order. (Not alphabetical, but reverse the current Array i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])
+// 1
+let a = fruits.shift();
+console.log(a);
+console.log(fruits);
 
-// // 3
-// fruits.push("Kiwi");
-// console.log(fruits);
+// 2
+console.log(fruits.sort());
 
-// // 4
-// let b = fruits.splice(0,1);
-// console.log(a);
-// console.log(fruits);
+// 3
+fruits.push("Kiwi");
+console.log(fruits);
 
-// // 5
-// console.log(fruits.reverse());
+// 4
+let b = fruits.splice(0,1);
+console.log(a);
+console.log(fruits);
+
+// 5
+console.log(fruits.reverse());
 
 
 
 
 // Exercise 2
+// Access and then console.log “Oranges”
 let moreFruits = ["Banana", 
     [
     "Apples", 
@@ -34,5 +41,5 @@ let moreFruits = ["Banana",
     ]
 ];
 
-let c = [moreFruits [1][1][0]];
+let c = moreFruits[1][1][0];
 console.log(c);
