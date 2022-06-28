170c456a8f4aaa1de705373142df28e102478da9
ElisavetaAlekseeva <ealekseeva515@gmail.com>
2022-06-28 08:04:15 +0300

    boom

--- Week2/Day1/ExercisesXPNinja/script.js
+++ Week2/Day1/ExercisesXPNinja/script.js
@@ -79,3 +79,36 @@ for (let k = 0; k < b.length; k++){
 if(!find){
     alert('~Nemo not found')
 }
+
+
+
+
+
+
+// Exercise 4 : Boom
+// Instructions
+// Hint: if statement (tomorrow’s lesson)
+// Prompt the user for a number. Depending on the users number you will return a string of the word “Boom”. Follow the rules below:
+// If the number given is less than 2 : return “boom
+// If the number given is bigger than 2 : the string should include n number of “o”s (n being the number given)
+// If the number given is evenly divisible by 2, add a exclamation mark to the end.
+// If the number given is evenly divisible by 5, return the string in ALL CAPS.
+// If the number given is evenly divisible by both 2 and 5, return the string in ALL CAPS and add an exclamation mark to the end.
+
+let user = Number(prompt('enter a number'));
+let str = '';
+if (user < 2) {
+    str = 'boom'
+} else {
+    for (let i = 0; i < user; i++) {
+        str += 'o' 
+    }
+}  
+if (user % 2 == 0){
+    str += '!';
+}  
+if(user % 5 == 0){
+    str = str.toUpperCase();
+}
+
+alert(str);
