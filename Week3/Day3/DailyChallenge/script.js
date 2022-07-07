d07434a7e11274ddfba19407966842af508c9107
ElisavetaAlekseeva <ealekseeva515@gmail.com>
2022-07-07 08:47:39 +0300

    redo

--- Week3/Day3/DailyChallenge/script.js
+++ Week3/Day3/DailyChallenge/script.js
@@ -4,25 +4,19 @@
 
 let body = document.getElementsByTagName('body')[0]
 let inpt = document.createElement('input')
-let val = inpt.value 
 document.body.appendChild(inpt)
 
-let btn = document.createElement('button')
-btn.innerHTML = 'click me'
-document.body.appendChild(btn)
-btn.addEventListener('click', subFun)
-    function subFun (event){
-        var inptVal = inpt.value
-       console.log(inptVal);
-    }
+// let btn = document.createElement('button')
+// btn.innerHTML = 'click me'
+// document.body.appendChild(btn)
+// btn.addEventListener('click', subFun)
+// function subFun (event){
+//     let inptVal = inpt.value
+//     let newStr = inptVal.replace(/[^a-zA-Z]/g,"")
+//     console.log(newStr);
+// }
 
-function removeNotLetters(inptVal) {
-    let inptValArray = inptVal.split('')
-    for (let i = 0; i < inptValArray.length; i++) {
-        if (!(64 < inptValArray[i] <  93) || !(96 < inptValArray[i] <  123)){
-            inptValArray = i.substring(0, i) + inptValArray.substring(i + 1)
-            i--
-        }
-        console.log(inptValArray);
-    }
-}
+inpt.addEventListener('keyup',function(e) {
+    this.value = this.value.replace(/[^a-zA-Z]/g,"")
+})
+
