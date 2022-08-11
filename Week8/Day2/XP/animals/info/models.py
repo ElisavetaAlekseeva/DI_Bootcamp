6ba6dc23229e80f52274bc072fe2419affce4117
ElisavetaAlekseeva <ealekseeva515@gmail.com>
2022-08-11 11:26:28 +0300

    .

--- Week8/Day2/XP/animals/info/models.py
+++ Week8/Day2/XP/animals/info/models.py
@@ -10,12 +10,12 @@ class Family(models.Model):
 
 class Animal(models.Model):
 
-    name = models.CharField(max_length=80,null=True)
+    name = models.CharField(max_length=50,null=True)
     legs = models.IntegerField()
     weight = models.IntegerField()
     height = models.IntegerField()
     speed = models.IntegerField()
-    family = models.ForeignKey(Family, on_delete = models.SET_NULL, null=True, related_name='posts')
+    family = models.ForeignKey(Family, on_delete = models.CASCADE, null=True)
 
     def __str__(self) -> str:
         return self.name
